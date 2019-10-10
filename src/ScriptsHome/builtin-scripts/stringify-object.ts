839ec284d3e54ef27a4154d8830e734c77e491c9
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-10-10 11:55:35 +0800

    [src/ScriptsHome/builtin-scripts] Implemented #jsonStringifyWithOptions.

---
+++ src/ScriptsHome/builtin-scripts/stringify-object.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+export interface IStringifyOptions {
+	minified?: boolean;
+
+	// separator: string;
+	useTabs?: boolean;
+	useSpaces?: boolean;
+
+	useSingleQuotes?: boolean;
+	noQuotesForKeys?: boolean;
+}
+
+export const jsonStringifyWithOptions = (o: object, {minified, useTabs, useSpaces, useSingleQuotes, noQuotesForKeys}: IStringifyOptions): string => {
+	const separator = minified ? undefined : (
+		useTabs ? '\t' : (
+			useSpaces ? ' '.repeat(4) : '\t'
+		)
+	);
+	// TO-DO Check out the usage of replacer.
+	let json = JSON.stringify(o, undefined, separator);
+	if (useSingleQuotes) {json = json.replace(/"/g, '\'');}
+	if (noQuotesForKeys) {
+		json = json.replace(/':/g, ':');
+		if (minified) {json = json.replace(/,'/g, ',');}
+		if (useTabs) {json = json.replace(/\t'/g, '\t');}
+		if (useSpaces) {json = json.replace(/ '/g, ' ');}
+	}
+	return json;
+};
