f4e56fc399668886be81cc2d1276221d5a14abf3
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-10-11 11:05:40 +0800

    [src/ScriptsHome/eval-scripts] Implemented #doReconstructStringByLines().

--- src/ScriptsHome/eval-scripts.ts
+++ src/ScriptsHome/eval-scripts.ts
@@ -5,9 +5,28 @@ interface IRes {
 	ex?: Error;
 }
 
+// Reconstruct string by custom line handler.
+export const doReconstructStringByLines = (input: string, handleLineScript: string, noEmptyLine: boolean = false, separator: string = '\n'): IRes => {
+	try {
+		// $line => '- ' + $line
+		const handler = eval(handleLineScript);
+		if (typeof handler !== 'function') {
+			return {ex: new Error('no line handler specified!')};
+		}
+		let lines = input.replace(/\r/g, '').split('\n');
+		if (noEmptyLine) {lines = lines.filter(line => Boolean(line && line.trim()));}
+		lines = lines.map(handler);
+		return {output: lines.join(separator)};
+	} catch (ex) {
+		return {ex};
+	}
+};
+
 export const doEvalScripts = (input: string, scripts: string): IRes => {
-	// @ts-ignore
 	const $input = input;
+	const $lines = $input.replace(/\r/g, '').split('\n');
+	// @ts-ignore
+	const $_lines = $lines.filter(line => Boolean(line && line.trim()));
 	// @ts-ignore
 	const $visualize = <T>(t: T): T => {
 		console.log('Visualizing the temp value:', t);
@@ -17,6 +36,7 @@ export const doEvalScripts = (input: string, scripts: string): IRes => {
 	if (!_scripts) {return {};}
 	try {
 		const output = eval(_scripts);
+		// Check out the user defined variables, like: $separator, $handleLine, and etc.
 		if (typeof output === 'function') {
 			const final = output($input);
 			return {output: final};
